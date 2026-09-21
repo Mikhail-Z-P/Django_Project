85fd2cc907344cf07c1b01c65fceff962ff7ee22
Sweetnes <999999zqq@gmail.com>
2026-09-22 01:40:49 +0700

    Реализованы product_detail, форма создания товара, валидация, переходы и подшаблон меню.

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('products/create/', views.product_create, name='product_create'),
 ]
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views.generic import TemplateView
 from .models import Product
-
+from django.shortcuts import redirect
+from .forms import ProductForm
 
 class HomeView(TemplateView):
     template_name = 'catalog/home.html'
@@ -15,3 +16,13 @@ class HomeView(TemplateView):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'catalog/product_detail.html', {'product': product})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/product_form.html', {'form': form})
