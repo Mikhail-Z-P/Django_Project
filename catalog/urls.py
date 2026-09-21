275ec8239e34fda1261eba98ed91c8a5efa231ed
Sweetnes <999999zqq@gmail.com>
2026-09-22 01:02:23 +0700

    сделан: view для детальной страницы товара, URL /products//, базовый шаблон и подшаблон меню переходы между страницами.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts"),
+    path('', views.HomeView.as_view(), name='home'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.views.generic import TemplateView
+from .models import Product
 
 
-def home(request):
-    return render(request, "catalog/home.html")
+class HomeView(TemplateView):
+    template_name = 'catalog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
 
 
-def contacts(request):
-    return render(request, "catalog/contacts.html")
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
